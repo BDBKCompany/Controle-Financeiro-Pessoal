fcb33696a433de39448b9e4b22c672d9492fdaa7
Boni <bonigabriel77@gmail.com>
2025-10-28 22:16:13 -0300

    criação da estrutura

---
+++ backend/BDBKCompany/src/main/java/com/curso/bdbkcompany/BdbkCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.curso.bdbkcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdbkCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdbkCompanyApplication.class, args);
+    }
+
+}
---
+++ backend/BDBKCompany/src/test/java/com/curso/bdbkcompany/BdbkCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.bdbkcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdbkCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
